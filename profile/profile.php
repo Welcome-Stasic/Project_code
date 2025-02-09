df81a038ae1d7002b93d929cc07313aba942697b
Papa-Jon <Stanislav.glumov.2006@mail.ru>
2025-02-09 22:41:30 +0500

    папка профиль для перехода после авторизации

---
+++ profile/profile.php
@@ -0,0 +1,25 @@
+<?php
+include("db/Users.php")
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <H1>
+        Профиль пользователя
+    </H1>
+    <?php
+    echo htmlspecialchars($user['name']);
+
+    ?>
+</body>
+
+</html>
