69a0aebaac4629de4949edd3998cd247080f946d
Papa-Jon <Stanislav.glumov.2006@mail.ru>
2025-02-09 22:42:50 +0500

    Сделал переход на профиль, и сохранение данных в куки

--- auth/db/authUsers.php
+++ auth/db/authUsers.php
@@ -1,87 +1,72 @@
 <?php
 include("db/db.php");
 
-
-
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $email = !empty($_GET['email']) ? $conn->real_escape_string(trim($_GET['email'])) : null;
-    $password = !empty($_GET['password']) ? $conn->real_escape_string(trim($_GET['password'])) : null;
+  $email = !empty($_GET['email']) ? $conn->real_escape_string(trim($_GET['email'])) : null;
+  $password = !empty($_GET['password']) ? $conn->real_escape_string(trim($_GET['password'])) : null;
 
+  $query = "SELECT * FROM users WHERE email = '$email'";
+  $result = $conn->query($query);
 
+  if ($result->num_rows > 0) {
+    $user = $result->fetch_assoc();
 
+    if (password_verify($password, $user['password'])) {
+      setcookie("username", $user['name'], time() + (86400 * 30), "/"); // 30 дней
+      setcookie("user_email", $user['email'], time() + (86400 * 30), "/"); // 30 дней
 
-    $query = "SELECT * FROM users WHERE email = '$email'";
-    $result = $conn->query($query);
-
-
-
-
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-
-
-
-        if (password_verify($password, $user['password'])) {
-            $alert_sucsses = "Добро пожаловать, " . htmlspecialchars($user['name']) . "!";
-            echo "<script>
-let timerInterval;
-Swal.fire({
-  icon: 'success',
-  title: '$alert_sucsses',
-  timer: 2000,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading();
-    const timer = Swal.getPopup().querySelector('b');
-    timerInterval = setInterval(() => {
-    }, 100);
-  },
-  willClose: () => {
-    clearInterval(timerInterval);
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log('I was closed by the timer');
-  }
-});
-            </script>";
-        } else {
-            echo "<script>
+      $alert_success = "Добро пожаловать, " . htmlspecialchars($user['name']) . "!";
+      echo "<script>
+            let timerInterval;
             Swal.fire({
-      title: 'Ой, Неверный пароль(',
-              icon: 'error',
-        showClass: {
-       popup: `
-      animate__animated
-      animate__fadeInUp
-      animate__faster
-       `
-      },
-      hideClass: {
-        popup: `
-          animate__animated
-          animate__fadeOutDown
-          animate__faster
-        `
-      }
-      });
+                icon: 'success',
+                title: '$alert_success',
+                timer: 2000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading();
+                    const timer = Swal.getPopup().querySelector('b');
+                    timerInterval = setInterval(() => {
+                    }, 100);
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                    window.location.href = '../profile/profile.php';
+                }
+            });
             </script>";
-        }
     } else {
-        echo "<script>
+      echo "<script>
+            Swal.fire({
+                title: 'Ой, Неверный пароль(',
+                icon: 'error',
+                showClass: {
+                    popup: `
+                        animate__animated
+                        animate__fadeInUp
+                        animate__faster
+                    `
+                },
+                hideClass: {
+                    popup: `
+                        animate__animated
+                        animate__fadeOutDown
+                        animate__faster
+                    `
+                }
+            });
+            </script>";
+    }
+  } else {
+    echo "<script>
         Swal.fire({
-        icon: 'error',
-        title: 'Ооой...',
-        text: 'Пользователь не найден!',
-        footer: '<a href='../index.html'>Похоже пора арегистрироваться</a>',
+            icon: 'error',
+            title: 'Ооой...',
+            text: 'Пользователь не найден!',
+            footer: '<a href='../index.html'>Похоже пора арегистрироваться</a>',
         });
         </script>";
-    }
-};
-
-
-
-
+  }
+}
 
 $conn->close();
