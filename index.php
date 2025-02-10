81752e0d58237267579c54f76bac2ddff7d1f25f
Стас <TimeToAgain@tutamail.com>
2025-02-10 05:41:01 +0500

    Создание приветственной страницы

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+</head>
+
+<body>
+    <a href="application/index.php">Регистрация</a>
+    <a href="application/registration.php">Авторизация</a>
+</body>
+
+</html>
